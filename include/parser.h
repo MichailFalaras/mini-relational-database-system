32d98a8f62702cb9e9efab49b8a2aeeb1811cd66
MichailFalaras <mixalisfalaras@gmail.com>
2026-07-05 17:29:00 +0300

    Update parser header: create Statement types/struct & update parser header

--- include/parser.h
+++ include/parser.h
@@ -3,15 +3,34 @@
 
 #include <stdint.h>
 #include "tokenizer.h"
+#include "ast.h"
+
+typedef enum statement_type {
+    STMT_CREATE_TABLE,
+    STMT_DROP_TABLE,
+    STMT_ALTER_TABLE,
+    STMT_TRUNCATE_TABLE,
+    STMT_CREATE_INDEX,
+    STMT_DROP_INDEX,
+    STMT_SELECT,
+    STMT_INSERT,
+    STMT_UPDATE,
+    STMT_DELETE
+} StatementType;
+
+typedef struct statement {
+    ASTNode *root;
+    StatementType type;
+} Statement;
 
 /* Parser component containing:
  * query_tokens: array of tokens
- * index: index of token array*/
+ * amount_tokens: amount of tokens in array
+ * current_position: current position while traversing array. */
 typedef struct parser {
     Token *query_tokens;
+    uint32_t amount_tokens;
     uint32_t current_position;
 } Parser;
-
-/* TODO: Abstract Syntax Tree Node Structs */
 
 #endif
