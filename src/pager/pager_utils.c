5753b9aa328c262dfe7375623022315c0f2c61d0
MichailFalaras <mixalisfalaras@gmail.com>
2026-07-23 12:31:27 +0300

    Refactor utils inteface with pread/pwrite for I/O and add status enums

--- src/pager/pager_utils.c
+++ src/pager/pager_utils.c
@@ -8,14 +8,17 @@
 #include "../../include/page.h"
 #include "pager_utils.h"
 
-/* Write exactly PAGE_SIZE data. */
+/* Write exactly PAGE_SIZE data using pwrite (without moving file offset). */
 bool write_page_size(Pager *pager, Page *page) {
-    int index = 0;
-    int bytes_left_to_write = PAGE_SIZE;
-    int bytes_written = 0;
+    size_t index = 0;
+    size_t bytes_left_to_write = PAGE_SIZE;
+    ssize_t bytes_written = 0;
+
+    off_t base_offset = (off_t) page->page_num * (off_t) PAGE_SIZE;
 
     while (bytes_left_to_write > 0){
-        bytes_written = write(pager->fd, page->page_data + index, bytes_left_to_write);
+        bytes_written = pwrite(pager->fd, page->page_data + index,
+                        bytes_left_to_write, base_offset);
 
         if (bytes_written < 0) {
             if (errno == EINTR) {
@@ -24,21 +27,28 @@ bool write_page_size(Pager *pager, Page *page) {
             return false;
         }
 
-        bytes_left_to_write -= bytes_written;
-        index += bytes_written;
+        if (bytes_written == 0) {
+            fprintf(stderr, "Possible zero byte loop terminated.\n");
+            return false;
+        }
+
+        bytes_left_to_write -= (size_t) bytes_written;
+        index +=  (size_t) bytes_written;
     }
 
     return true;
 }
 
-/* Read exacty PAGE_SIZE data. */
+/* Read exacty PAGE_SIZE data using pread (without moving file offset). */
 bool read_page_size(Pager *pager, Page *page) {
-    int index = 0;
-    int bytes_left_to_read = PAGE_SIZE;
-    int bytes_read = 0;
+    size_t index = 0;
+    size_t bytes_left_to_read = PAGE_SIZE;
+    ssize_t bytes_read = 0;
+
+    off_t base_offset = (off_t) page->page_num * (off_t) PAGE_SIZE;
 
     while (bytes_left_to_read > 0){
-        bytes_read = read(pager->fd, page->page_data + index, bytes_left_to_read);
+        bytes_read = pread(pager->fd, page->page_data + index, bytes_left_to_read, base_offset);
 
         if (bytes_read < 0) {
             if (errno == EINTR) {
@@ -47,22 +57,27 @@ bool read_page_size(Pager *pager, Page *page) {
             return false;
         }
 
-        bytes_left_to_read -= bytes_read;
-        index += bytes_read;
+        if (bytes_read == 0) {
+            fprintf(stderr, "read_page_size has reached EOF.\n");
+            break;
+        }
+
+        bytes_left_to_read -= (size_t) bytes_read;
+        index += (size_t) bytes_read;
     }
 
    return true;
 }
 
 /* Get first free page from Free List Head. */
-bool get_free_page(Pager *pager, uint32_t *page_num) {
+FreePageResult get_free_page(Pager *pager, uint32_t *page_num) {
     if (!pager || !page_num) {
-        return false;
+        return FREE_PAGE_ERROR;
     }
 
     Page *zero = pager_get_page(pager, 0);
     if (!zero) {
-        return false;
+        return FREE_PAGE_ERROR;
     }
 
     uint32_t free_list_head;
@@ -72,21 +87,25 @@ bool get_free_page(Pager *pager, uint32_t *page_num) {
     
     /* No free pages. */
     if (free_list_head == 0) {
-        return false;
+        return FREE_PAGE_EMPTY;
+    } else if (free_list_head >= pager->num_pages || free_list_head >= MAX_PAGES) {
+        fprintf(stderr, "Corrupted Free List page number.\n");
+        return FREE_PAGE_ERROR;
     }
 
     /* Get page from cache or disk. */
     Page *free_page = pager_get_page(pager, free_list_head);
     if (!free_page) {
-        return false;
+        return FREE_PAGE_ERROR;
     }
     
     *page_num = free_list_head;
-    
-    if (!page_mark_dirty(zero)) {
-        return false;
+    if (!page_clear(pager, pager->pages[*page_num])) {
+        page_free(free_page);
+        pager->pages[free_list_head] = NULL;
+        return FREE_PAGE_ERROR;
     }
-
+    
     /* Since we are returning the first free page no matter what,
      * update free_list_head of page 0 to show to the free page after
      * the page we are returning. */
@@ -94,18 +113,26 @@ bool get_free_page(Pager *pager, uint32_t *page_num) {
         free_page->page_data,
         sizeof(uint32_t));
 
-    return true;
+
+    if (!page_mark_dirty(zero)) {
+        page_free(free_page);
+        pager->pages[free_list_head] = NULL;
+        return FREE_PAGE_ERROR;
+    }
+
+    return FREE_PAGE_FOUND;
 }
 
 /* Check if page is already free. */
-bool is_page_free(Pager *pager, uint32_t page_num) {
-    if (!pager || page_num >= MAX_PAGES) {
-        return false;
+PageFreeStatus is_page_free(Pager *pager, uint32_t page_num) {
+    if (!pager || page_num >= pager->num_pages 
+        || page_num >= MAX_PAGES) {
+        return PAGE_FREE_ERROR;
     }
 
     Page *zero = pager_get_page(pager, 0);
     if (!zero) {
-        return false;
+        return PAGE_FREE_ERROR;
     }
 
     uint32_t free_list_head;
@@ -115,15 +142,29 @@ bool is_page_free(Pager *pager, uint32_t page_num) {
         sizeof(uint32_t)
     );
 
+    if (free_list_head >= pager->num_pages || free_list_head >= MAX_PAGES) {
+        fprintf(stderr, "Corrupted Free List page number.\n");
+        return PAGE_FREE_ERROR;
+    }
+
+    uint32_t visited = 0;
     while (free_list_head != 0) {
+        visited++;
+
+        /* Check if Free List cycle is endless. */
+        if (visited > pager->num_pages) {
+            fprintf(stderr, "Terminating is_page_free loop.\n");
+            return PAGE_FREE_ERROR;
+        }
+
         if (free_list_head == page_num) {
-            return true;
+            return PAGE_IS_FREE;
         }
 
         if (!pager->pages[free_list_head]) {
             /* Something wrong with free list pages then. */
             if (!pager_get_page(pager, free_list_head)) {
-                return false;
+                return PAGE_FREE_ERROR;
             }
             /* Load the page into memory and continue the loop. */
         }
@@ -135,13 +176,14 @@ bool is_page_free(Pager *pager, uint32_t page_num) {
         );
     }
 
-    return false;
+    return PAGE_NOT_FREE;
 }
 
 /* Connect free page to the beginning of the Free Page List. */
 bool connect_free_page(Pager *pager, uint32_t page_num) {
 
-    if (!pager || page_num >= pager->num_pages) {
+    if (!pager || page_num >= pager->num_pages
+        || page_num >= MAX_PAGES) {
         return false;
     }
 
@@ -155,7 +197,10 @@ bool connect_free_page(Pager *pager, uint32_t page_num) {
         return false;
     }
 
-    if (is_page_free(pager, page_num)) {
+    PageFreeStatus res = is_page_free(pager, page_num); 
+    if (res == PAGE_FREE_ERROR) {
+        return false;
+    } else if (res == PAGE_IS_FREE) {
         fprintf(stderr, "Page already free.\n");
         return false;
     }
@@ -167,6 +212,11 @@ bool connect_free_page(Pager *pager, uint32_t page_num) {
         sizeof(uint32_t)
     );
 
+    if (free_list_head >= pager->num_pages || free_list_head >= MAX_PAGES) {
+        fprintf(stderr, "Corrupted Free List page number.\n");
+        return false;
+    }
+
     memcpy(
         zero->page_data + FREE_LIST_HEAD_OFFSET,
         &page_num,
@@ -174,12 +224,12 @@ bool connect_free_page(Pager *pager, uint32_t page_num) {
     );
 
     memcpy(
-        pager->pages[page_num]->page_data,
+        page->page_data,
         &free_list_head,
         sizeof(uint32_t)
     );
 
-    if (!page_mark_dirty(zero) || !page_mark_dirty(pager->pages[page_num])) {
+    if (!page_mark_dirty(zero) || !page_mark_dirty(page)) {
         return false;
     }
 
--- src/pager/pager_utils.h
+++ src/pager/pager_utils.h
@@ -5,14 +5,16 @@
 
 typedef struct pager Pager;
 typedef struct page Page;
+typedef enum free_page_result FreePageResult;
+typedef enum page_free_status PageFreeStatus;
 
 extern bool write_page_size(Pager *pager, Page *page);
 
 extern bool read_page_size(Pager *pager, Page *page);
 
-extern bool is_page_free(Pager *pager, uint32_t page_num);
+extern FreePageResult get_free_page(Pager *pager, uint32_t *page_num);
 
-extern bool get_free_page(Pager *pager, uint32_t *page_num);
+extern PageFreeStatus is_page_free(Pager *pager, uint32_t page_num);
 
 extern bool connect_free_page(Pager *pager, uint32_t page_num);
 
