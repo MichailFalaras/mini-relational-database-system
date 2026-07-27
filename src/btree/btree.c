2ec3270651441b5e0b3d6539232f45f4556b6fad
MichailFalaras <mixalisfalaras@gmail.com>
2026-07-27 15:53:21 +0300

    Refactor BTree interface using BTree node layout accessors

--- src/btree/btree.c
+++ src/btree/btree.c
@@ -6,8 +6,9 @@
 #include "../../include/data_types.h"
 #include "../../include/row.h"
 #include "../../include/index.h"
-#include "../../include/execution_engine.h"
+//#include "../../include/execution_engine.h" might need to move to index.h
 #include "../src/data_types/data_types_utils.h"
+#include "btree_utils.h"
 
 /* After btree_init the pages should be marked dirty via Pager. */
 
@@ -55,27 +56,44 @@ bool btree_init_internal(void *page_data, uint32_t rightmost_child_pointer) {
  * pointers to other pages. */
 uint16_t btree_get_available_capacity(void *page_data) {
     if (!page_data) {
-        return 0;
+        return UINT16_MAX;
     }
 
     uint16_t reserved_space = 0;
-    BTreeCommonHeader *common_header = (BTreeCommonHeader *) page_data;
+    uint8_t node_type;
+    uint16_t cell_count;
+    uint16_t free_space_offset;
+    if (!get_node_type(page_data, &node_type)){
+        return UINT16_MAX;
+    }
 
-    if (common_header->node_type == 1) {
+    if (!get_cell_count(page_data, &cell_count)) {
+        return UINT16_MAX;
+    }
+
+    if (!get_free_space_offset(page_data, &free_space_offset)) {
+        return UINT16_MAX;
+    }
+
+    if (node_type) {
         reserved_space += BTREE_INTERNAL_NODE_SIZE;
     } else {
         reserved_space += BTREE_LEAF_NODE_SIZE;
     }
 
-    reserved_space += common_header->cell_count * 2;
+    reserved_space += cell_count * 2;
 
-    return common_header->free_space_offset - reserved_space;
+    return free_space_offset - reserved_space;
 }
 
 /* Check if there's enough space to store payload + its pointer/offset
  * in the available space. */
 bool btree_has_enough_space(void *page_data, uint16_t payload_size) {
     uint16_t available_space = btree_get_available_capacity(page_data);
+    if (available_space == UINT16_MAX) {
+        return false;
+    }
+
     uint16_t needed_space = payload_size + 2;
 
     if (available_space >= needed_space) {
@@ -91,12 +109,12 @@ bool btree_compare(Value **values, const void *key, void *context, int *result) 
         return false;
     }
 
-    KeyExtractionContext *key_extraction_ctx = (KeyExtractionContext *) context;
+    KeyExtractionContext *ctx = (KeyExtractionContext *) context;
     const Value **key_val = (Value **) key;
 
     /* Compare both same index columns. (With upper limit the search keys used, not
     the amount of keys the BTree is organized with) */
-    for (uint32_t i = 0; i < key_extraction_ctx->num_search_keys; i++) {
+    for (uint32_t i = 0; i < ctx->num_search_keys; i++) {
         if (!value_compare(values[i], key_val[i], result)) {
             return false;
         }
@@ -113,55 +131,24 @@ bool btree_compare(Value **values, const void *key, void *context, int *result) 
     return true;
 }
 
-/* Extract data from either Internal/Leaf node.*/
-Value **btree_extract_data(uint16_t node_type, const void *data_offset, void *context) {
-    if (!data_offset || !context) {
+/* Extract keys from either Internal/Leaf node.*/
+Value **btree_extract_data(void *page_data, uint16_t cell_pointer, void *context) {
+    if (!page_data || !context) {
         return NULL;
     }
 
-    KeyExtractionContext *key_extraction_ctx = (KeyExtractionContext *) context;
-    Value **values = NULL;
-    uint8_t *data = (uint8_t *) data_offset;
-
-    /* For internal nodes: */
-    if (node_type == 1) {
-         values = (Value **) malloc(key_extraction_ctx->index_key->num_columns*sizeof(Value *));
-        if (!values) {
-            return NULL;
-        }
-
-        // Skip the child pointer
-        data += (uint8_t) 4;
-        for (uint32_t i = 0; i < key_extraction_ctx->index_key->num_columns; i++) {
-            // According to the column's DataType, create a value and store it.
-            values[i] = value_create(key_extraction_ctx->data_types[i], data);
-            if (!values[i]) {
-                for (uint32_t j = 0; j < i; j++) {
-                    value_free(values[j]);
-                }
-                free(values);
-                return NULL;
-            }
-
-            // Skip the exact size of the column's DataType.
-            data += (uint8_t) get_data_type_size(key_extraction_ctx->data_types[i]);
-        }
-
-    /* For leaf nodes: */
-    } else {
-        Row *row = (Row *) data;
-
-        /* Extract row's exact values. */
-        values = row_get_values(row, key_extraction_ctx->index_key->column_index_array,
-                                        key_extraction_ctx->index_key->num_columns);
-        if (!values) {
-            return NULL;
-        }
+    uint8_t node_type;
+    if (!get_node_type(page_data, &node_type)) {
+        return NULL;
     }
 
-    return values;
-}
+    void *ids;
+    if (!get_cell_id(page_data, cell_pointer, context, &ids)) {
+        return NULL;
+    }
 
+    return (Value **) ids;
+}
 
 /* Lower Bound Binary Search.
  * ---> Execution Engine calling this function.
@@ -175,44 +162,47 @@ uint16_t btree_lower_bound(void *page_data, const void *key, void *context) {
         return UINT16_MAX;
     }
 
-    KeyExtractionContext *key_extraction_ctx = (KeyExtractionContext *) context;
-
-    /* Initialize from and to and mid.*/
-    BTreeCommonHeader *common_header = (BTreeCommonHeader *) page_data;
-    uint16_t from = 0;
-    if (common_header->cell_count == 0) {
+    uint16_t cell_count;
+    uint8_t node_type;
+    if (!get_node_type(page_data, &node_type)) {
         return UINT16_MAX;
     }
-    uint16_t to = common_header->cell_count - 1;
+    if (!get_cell_count(page_data, &cell_count)) {
+        return UINT16_MAX;
+    }
+
+    if (cell_count == 0) {
+        return UINT16_MAX;
+    }
+
+    KeyExtractionContext *ctx = (KeyExtractionContext *) context;
+
+    /* Initialize from and to and mid.*/
+    uint16_t from = 0;
+    uint16_t to = cell_count - 1;
     uint16_t mid = 0;
 
     /* Initialize cell pointer local variable to hold cell's offset in page.*/
     uint16_t cell_pointer = 0;
 
-    /* Calculate whole header offset for internal/leaf nodes. */
-    uint16_t header_offset = sizeof(BTreeCommonHeader) +
-             (common_header->node_type ? BTREE_INTERNAL_NODE_SIZE : BTREE_LEAF_NODE_SIZE);
-
-    /* Initialize pointer showing to that offset in page data. */
-    uint16_t *offset = (uint16_t *) ((uint8_t *)page_data + header_offset);
-    
     /* Result local var to hold btree_compare results. */
     int result;
 
     /* result_index to hold found key index position of page. */
     uint16_t result_index = UINT16_MAX;
-    uint8_t *data_offset; // Active moving pointer throughout the loop.
+    //uint8_t *data_offset; Active moving pointer throughout the loop.
     Value **values = NULL;
 
     while (from <= to) {
         mid = (from + to)/2;
 
-        memcpy(&cell_pointer, offset + mid, sizeof(uint16_t));
-        data_offset = (uint8_t *)page_data + cell_pointer;
-        
+        if (!get_cell_pointer(page_data, mid, &cell_pointer)) {
+            result_index = UINT16_MAX;
+            break;
+        }
+
         /* Extract data from either internal/leaf node. */
-        values = btree_extract_data(common_header->node_type, data_offset,
-                                            context);
+        values = btree_extract_data(page_data, cell_pointer, context);
         if (!values) {
             result_index = UINT16_MAX;
             break;
@@ -221,17 +211,16 @@ uint16_t btree_lower_bound(void *page_data, const void *key, void *context) {
         /* Compare columns lexicographically. */
         if (!btree_compare(values, key, context, &result)) {
             result_index = UINT16_MAX;
+            value_free_array(values, ctx->index_key->num_columns);
             break;
         }
-
-        // Looking to keep any columns that are >= key.
-    
-        /* Meaning all columns were equal. Then update result index.
-         * But continue the loop just in case you find the LOWER BOUND. */
+       
+        /* Continue the loop just in case you find the LOWER BOUND. */
         if (result >= 0) {
             result_index = mid;
 
             if (mid == 0) {
+                value_free_array(values, ctx->index_key->num_columns);
                 break;
             }
             to = mid - 1;
@@ -240,23 +229,8 @@ uint16_t btree_lower_bound(void *page_data, const void *key, void *context) {
         } 
 
         /* Free temporary values array in each loop. */
-        if (values) {
-            for (uint32_t i = 0; i < key_extraction_ctx->index_key->num_columns; i++) {
-                value_free(values[i]);
-            }
-            free(values);
-            values = NULL;
-        }
+        value_free_array(values, ctx->index_key->num_columns);
     }
     
-    /* Same if loop ended from error. */
-    if (values) {
-        for (uint32_t i = 0; i < key_extraction_ctx->index_key->num_columns; i++) {
-            value_free(values[i]);
-        }
-        free(values);
-        values = NULL;
-    }
-
     return result_index;
 }
