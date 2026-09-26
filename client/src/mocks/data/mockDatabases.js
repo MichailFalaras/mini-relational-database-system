c4dfcd4a086d981a1088e6a084a34e7a75eb5715
Apostolis Falaras <apostolisfalaras@gmail.com>
2026-09-26 21:58:43 +0300

    Add isDemo field to check if the loaded database is the demo database or not

--- client/src/mocks/data/mockDatabases.js
+++ client/src/mocks/data/mockDatabases.js
@@ -2,8 +2,8 @@ export const MOCK_DATABASES = [
     {
 		id: 1, 
 		name: "e_commercedb", 
-		status: "connected", 
-		user: "apostolis", 
+		status: "open", 
+		isDemo: true, 
 		numTables: 6, 
 		size: "7.2 MB"
 	}
